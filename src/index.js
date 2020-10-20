c62ca8cec8487c9889df9babe3ed2d2b875ce441
Misha Memus <fedarovichmikhail@gmail.com>
2020-10-20 03:50:09 +0300

    сверстанна вторая страница

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const tooltip = document.querySelector('.news-card__tooltip')
 const saveIcon = document.querySelectorAll('.news-card__save-icon')
+const deleteIcon = document.querySelectorAll('.news-card__delete-icon')
 
 
 saveIcon.forEach(icon => {
@@ -15,5 +16,19 @@ saveIcon.forEach(icon => {
     tip.style.display = 'none'
   })
 })
+deleteIcon.forEach(icon => {
+  icon.addEventListener('mouseover', (evt) => {
+    const tip = evt.relatedTarget.querySelector('.news-card__tooltip')
+    console.log(tip)
+    tip.style.display = 'flex'
+
+  })
+  icon.addEventListener('mouseout', (evt) => {
+    const tip = evt.relatedTarget.querySelector('.news-card__tooltip')
+    console.log(tip)
+    tip.style.display = 'none'
+  })
+})
+
 
 
